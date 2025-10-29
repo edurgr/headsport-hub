60ba9d136fde1d34ce15f684f7fc0413ead05a5d
Edur Gorospe Roldan <edurgr@gmail.com>
2025-10-29 13:52:50 +0100

    fix(opennext): set converter to 'edge' for cloudflare wrappers

--- app/open-next.config.ts
+++ app/open-next.config.ts
@@ -2,11 +2,13 @@ export default {
   default: {
     override: {
       wrapper: 'cloudflare-node',
+      converter: 'edge',
     },
   },
   middleware: {
     override: {
       wrapper: 'cloudflare-edge',
+      converter: 'edge',
     },
   },
 };
